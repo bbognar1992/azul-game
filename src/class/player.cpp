756466cea342cfe07b927fbcfd3898e49e89376d
Bognár Bence <beni20180722@gmail.com>
2020-03-19 21:41:46 +0100

    updated player class

--- src/class/player.cpp
+++ src/class/player.cpp
@@ -6,16 +6,58 @@ using namespace std;
 class Player
 {
   private:
+  string name;
   int points;
 
-  int** wallTiles;
-  int** sampleTiles;
-  int* floorTiles;
+  int[5][5] wallTiles;
+  int[5][2] sampleTiles; //típus és darab
+  int[7] floorTiles;
 
-  void addPoint(int newPoint);
+  void addPoints(int newPoints){
+    this.points+=newPoints;
+  }
 
   public:
-  int getPoint();
-  int putTileToSampleTiles(int** disks, int* freeTiles);
-  int putSamplesToWall(int* rest_of_tiles);
+  void Player(string name){
+    this.name = name;
+    this.points = 0;
+    int i,j;
+    for (i = 0; i <= 5; i++)
+         for (j = 0; j < = 5; j++)
+            this.wallTiles[i][j] = 0;
+
+    for (i = 0; i <= 5; i++)
+        for (j = 0; j < = 2; j++)
+            this.sampleTiles[i][j] = 0;
+
+    for (i = 0; i <= 5; i++)
+        this.floorTiles[i] = 0;
+  }
+  int getPoint(){
+    return this.points;
+  }
+  int putTileToSampleTiles(int** disks, int* freeTiles){
+    int input;
+    firstQuestion:
+    cout << this.name << ": Do you want to choose from disk[0] or centre[1]?\n"
+    cin>>input;
+
+    if(input==0){
+      cout << this.name << ": Which disk?\n"
+      cin>>input;
+      cout << this.name << ": Which tiles?\n"
+      cin>>input;
+    }
+    else if(input==1){
+
+    }
+    else{
+      goto firstQuestion;
+    }
+
+
+  }
+  int putSamplesToWall(int* freeTiles){
+
+  }
 };
